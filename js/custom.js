318f311d282351892db44808c56ceceb8c11b075
felix <felix@volny.io>
2015-09-15 13:44:31 +0700

    better map

--- js/custom.js
+++ js/custom.js
@@ -26,3 +26,15 @@ $(function() {
     }
   });
 });
+
+// Map scrolling behaviour
+$(document).ready(function() {
+  $('#map_iframe').addClass('scrolloff');
+  $('#map').on('click', function () {
+    $('#map_iframe').removeClass('scrolloff');
+  });
+
+  $('#map_iframe').mouseleave(function  () {
+    $('#map_iframe').addClass('scrolloff');
+  });
+});
